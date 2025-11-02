16a3ca614159dfb46f54849bb20b275add3e8d87
Lomica Inside <lomicainside@gmail.com>
2025-11-02 14:37:43 +0200

    Update version to 0.56 in __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Odd Faces Detector",
     "author": "Lomica Inside",
-    "version": (0, 5),
+    "version": (0, 56),
     "blender": (2, 80, 0),
     "location": "View3D > Sidebar > Odd Faces Detector",
     "description": "This basic Blender add-on makes modeling significantly easier by detecting and counting tris and n-gons in edit mode, with face selection buttons for each, saving the time wasted by looking for them.",
